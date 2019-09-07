ee8087a5435be2719730750e643d865e57449ff0
Kota Nishinaka <private2030kota@gmail.com>
2019-09-07 11:27:13 +0900

    Making an Interactive Component

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
   render() {
-    return <button className='square'>{this.props.value}</button>;
+    return (
+      <button className='square' onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
