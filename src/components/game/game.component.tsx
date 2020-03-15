0276d24c1d43025178ea0037bd8f3a1bb3ece279
Kota Nishinaka <knishina@yahoo-corp.jp>
2020-03-15 18:09:00 +0900

    Add prettier and eslint

--- src/components/game/game.component.tsx
+++ src/components/game/game.component.tsx
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-import Board from "../board/board.component";
-import calculateWinner from "../../utils/calculateWinner";
+import Board from '../board/board.component'
+import calculateWinner from '../../utils/calculateWinner'
 
 const Game = () => {
-  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
-  const [xIsNext, setXIsNext] = useState(true);
-  const [stepNumber, setStepNumber] = useState(0);
-  const [maxTurn, setMaxTurn] = useState(0);
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }])
+  const [xIsNext, setXIsNext] = useState(true)
+  const [stepNumber, setStepNumber] = useState(0)
+  const [maxTurn, setMaxTurn] = useState(0)
 
   const handleClick = (i: number) => {
-    const newHistory = history.slice(0, stepNumber + 1);
-    const current = newHistory[newHistory.length - 1];
-    const squares = current.squares.slice();
+    const newHistory = history.slice(0, stepNumber + 1)
+    const current = newHistory[newHistory.length - 1]
+    const squares = current.squares.slice()
 
     if (calculateWinner(squares) || squares[i]) {
-      return;
+      return
     }
-    squares[i] = xIsNext ? "X" : "O";
+    squares[i] = xIsNext ? 'X' : 'O'
 
     setHistory(
       newHistory.concat([
@@ -25,23 +25,23 @@ const Game = () => {
           squares: squares
         }
       ])
-    );
-    setStepNumber(newHistory.length);
-    setXIsNext(!xIsNext);
-    setMaxTurn(newHistory.length);
-  };
+    )
+    setStepNumber(newHistory.length)
+    setXIsNext(!xIsNext)
+    setMaxTurn(newHistory.length)
+  }
 
   const jumpTo = (step: number) => {
-    setStepNumber(step);
-    setMaxTurn(step);
-    setXIsNext(step % 2 === 0);
-  };
+    setStepNumber(step)
+    setMaxTurn(step)
+    setXIsNext(step % 2 === 0)
+  }
 
-  const current = history[stepNumber];
-  const winner = calculateWinner(current.squares);
+  const current = history[stepNumber]
+  const winner = calculateWinner(current.squares)
 
   const moves = history.map((step, move) => {
-    const desc = move ? `Go to move #${move}` : "Go to game start";
+    const desc = move ? `Go to move #${move}` : 'Go to game start'
 
     return (
       <li key={move}>
@@ -54,15 +54,15 @@ const Game = () => {
           {desc}
         </button>
       </li>
-    );
-  });
+    )
+  })
 
-  let status;
+  let status
 
   if (winner) {
-    status = `Winner: ${winner}`;
+    status = `Winner: ${winner}`
   } else {
-    status = `Next player: ${xIsNext ? "X" : "O"}`;
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`
   }
 
   return (
@@ -75,7 +75,7 @@ const Game = () => {
         <ol>{moves}</ol>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Game;
+export default Game
